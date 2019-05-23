10d478d8a6e0cc26de88e2921dcee5a3115e02c1
Marco J <marco.jiralerspong@mail.mcgill.ca>
2019-05-23 16:26:14 -0400

    Updated graphs being created to include plots of AF by population for different categories

--- scripts/create_graphs.py
+++ scripts/create_graphs.py
@@ -4,51 +4,187 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-""" SETUP DATA """
+def create_df(tsv, extra):
+    """
+    :param tsv: Path to .tsv
+    :param extra: Tuple for extra column to include in dictionary. (name, tsv_column_name)
+    :return: Dataframe where each row has columns "pop", "var_id", extra and "AF"
+    """
+
+    variants = pd.read_csv(tsv, sep='\t')
+
+    dicts = []
+    for index, row in variants.iterrows():
+        for population in populations:
+            dict = {}
+            dict['Population'] = population
+            dict['Allele Frequency'] = row['AC_' + population] / row['AN_' + population]
+            dict[extra[0]] = row[extra[1]]
+            dicts.append(dict)
+
+    graph_inesss = pd.DataFrame(dicts)
+
+    return graph_inesss
+
 
 populations = ["eas", "afr", "amr", "asj", "sas", "nfe", "fin"]
-filtered_variants = pd.read_csv('../analysis/filtered_variants.tsv', sep='\t')
-filtered_variants_inesss = filtered_variants.loc[filtered_variants['INESSS'] == True] # Only inesss variants
-
-freq_lof = {}
-freq_inesss = {}
-
-# Get individual population allele frequencies
-for population in populations:
-    ac_pop = 'AC_' + population
-    an_pop = 'AN_' + population
-
-    # To deal with NaN, get indices where they are NaN and only sum when not NaN (issue with sas pop)
-    null_index = filtered_variants[an_pop].map(np.isnan)
-    notnull_index = [not i for i in null_index]
-
-    # Compute frequency over all variants for only inesss as well as all
-    freq_inesss[population] = np.sum(filtered_variants_inesss[ac_pop].map(int) / filtered_variants_inesss[an_pop].map(int))
-    freq_lof[population] = np.sum(filtered_variants[ac_pop][notnull_index].map(int) / filtered_variants[an_pop][notnull_index].map(int))
-
-# Transform to dataframe
-freq_dict = {
-    'populations': populations,
-    'freq_lof': list(freq_lof.values()),
-    'freq_inesss': list(freq_inesss.values())
-}
-freq_df = pd.DataFrame.from_dict(freq_dict)
-
-# Get relative proportion of inesss over all variants
-freq_df['inesss_lof_freq'] = freq_df['freq_inesss']/freq_df['freq_lof']
 
 
-""" PLOT DATA """
+""" SEABORN SETTINGS """
 
+custom = ["#ec6b2d", "#00aabb","#ff5959", "#545454", "#fbae17", "#34495e", "#2ecc71"]
+sns.set_palette(sns.color_palette(custom))
 sns.set_style("darkgrid")
-sns.barplot(x='populations',y = 'freq_lof', data=freq_df, color = "skyblue")
-sns.barplot(x='populations',y = 'freq_inesss', data=freq_df, color = "pink")
 
-plt.xlabel('Population')
-plt.ylabel('Frequency')
-# plt.title('Allele frequence of LOF variants of DPYD in different populations and proportion covered by INESSS variants')
 
-for index, row in freq_df.iterrows():
-    txt = str(round(row['inesss_lof_freq'] * 100, 2)) + " %"
-    plt.text(index, row['freq_lof'], txt, ha = "center")
+""" INESSS GRAPH """
+
+graph_inesss = create_df('../analysis/inesss_variants.tsv', ('VAR_ID', 'VAR_ID'))
+
+ax = sns.catplot(data=graph_inesss, x='Population', y='Allele Frequency', hue="VAR_ID", kind='bar', legend=False, aspect=2, height=9, hue_order=['1-97981343-A-C', '1-97547947-T-A' ,'1-97915614-C-T', '1-98039419-C-T'])
+
+plt.legend(loc='upper left')
+plt.show()
+
+
+"""" CLIN GRAPH """
+
+graph_clin = create_df('../analysis/clin_variants.tsv', ('CLIN_SIG', 'CLIN_SIG'))
+
+ax = sns.catplot(data=graph_clin, x='Population', y='Allele Frequency', hue="CLIN_SIG", kind='swarm', legend=False, aspect=1.5, height=5)
+plt.ylim(0,0.002)
+plt.legend(loc='upper right')
+plt.show()
+
+
+"""" LOF GRAPH """
+
+graph_LOF = create_df('../analysis/lof_variants.tsv', ('LOF', 'LOF'))
+
+ax = sns.catplot(data=graph_inesss, x='POP', y='AF', hue="LOF", kind='swarm', legend=False, aspect=1.5, height=5)
+plt.ylim(0,0.0005)
+plt.legend(loc='upper right')
+plt.show()
+
+
+""" COMBINED GRAPH """
+
+all_variants = pd.read_csv('../analysis/all_variants.tsv', sep='\t')
+
+dicts = []
+for index, row in all_variants.iterrows():
+    for population in populations:
+        dict = {}
+        dict['Population'] = population
+        dict['VAR_ID'] = row['VAR_ID']
+
+        if row['INESSS'] == True:
+            second_cat = 'INESSS'
+        elif row['CLIN_SIG'] in ["Pathogenic", "Likely_pathogenic" , "Pathogenic/Likely_pathogenic"]:
+            second_cat = 'CLIN_VAR'
+        else:
+            second_cat = 'LOF'
+
+        dict['SECOND'] = second_cat
+        dict['Allele Frequency'] = row['AC_' + population] / row['AN_' + population]
+        dicts.append(dict)
+
+graph_inesss = pd.DataFrame(dicts)
+
+ax = sns.catplot(data=graph_inesss, x='Population', y='Allele Frequency', hue="SECOND", kind='swarm', legend=False, aspect = 1.5)
+plt.legend(loc='upper right')
+plt.ylim(0,0.03)
+plt.show()
+
+
+""" INESSS VS CLINVAR VS LOF BY POPULATION """
+
+# Get variants
+inesss_variants = pd.read_csv('../analysis/inesss_variants.tsv', sep='\t')
+clin_variants = pd.read_csv('../analysis/clin_variants.tsv', sep='\t')
+lof_variants = pd.read_csv('../analysis/lof_variants.tsv', sep='\t')
+
+dicts = []
+
+# Add each inesss variant
+for index, row in inesss_variants.iterrows():
+    for population in populations:
+        dict = {}
+        dict['Population'] = population
+        dict['Source'] = row['VAR_ID']
+        dict['Allele Frequency'] = row['AC_' + population] / row['AN_' + population]
+        dicts.append(dict)
+
+# Count total AF for the ClinVar variants
+totals = {population: 0 for population in populations}
+for index, row in clin_variants.iterrows():
+    for population in populations:
+        totals[population] += row['AC_' + population] / row['AN_' + population]
+
+# Add entries
+for population in populations:
+    dicts.append({
+        'Source': 'ClinVar',
+        'Population': population,
+        'Allele Frequency': totals[population]
+    })
+
+# Count total AF for LOF variants
+totals = {population: 0 for population in populations}
+for index, row in lof_variants.iterrows():
+    for population in populations:
+        totals[population] += row['AC_' + population] / row['AN_' + population]
+
+# Add entries
+for population in populations:
+    dicts.append({
+        'Source': 'LOF',
+        'Population': population,
+        'Allele Frequency': totals[population]
+    })
+
+ax = sns.catplot(data=pd.DataFrame(dicts), x='Population', y='Allele Frequency', hue='Source', kind='bar', legend=False, aspect=2)
+plt.legend(loc='upper left')
+plt.ylim(0,0.03)
+plt.show()
+
+
+""" ENTIRE POPULATION GRAPH """
+
+dicts = []
+
+# Add each inesss variant
+for index, row in inesss_variants.iterrows():
+    dict = {}
+    dict['ID'] = row['VAR_ID']
+    dict['Allele Frequency'] = row['AC'] / row['AN']
+    dicts.append(dict)
+
+# Get total for ClinVar variants
+total = 0
+for index, row in clin_variants.iterrows():
+    total += (row['AC'] / row['AN'])
+
+# Add entry
+dicts.append({
+    'ID': 'ClinVar',
+    'AF': total
+})
+
+# Get total for LOF variants
+total = 0
+for index, row in lof_variants.iterrows():
+    total += row['AC'] / row['AN']
+
+# Add entry
+dicts.append({
+    'ID': 'LOF',
+    'AF': total
+})
+
+graph_inesss = pd.DataFrame(dicts)
+
+ax = sns.catplot(data=graph_inesss, x='ID', y='Allele Frequency', kind='bar', legend=False, aspect=2)
+plt.legend(loc='upper right')
+plt.ylim(0,0.015)
 plt.show()
